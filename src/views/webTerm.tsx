e5d0f346f7f755579d746e7e2591d79be4752736
Penguin <zliyong@gmail.com>
2017-04-23 16:59:48 -0700

    enable web terminal

--- src/views/webTerm.tsx
+++ src/views/webTerm.tsx
@@ -4,6 +4,12 @@ import * as ReactDOM from 'react-dom';
 export class WebTerm extends React.Component<any, any>{
     private term: any;
     private pid: string;
+
+    constructor(props: any) {
+        super(props);
+        this.onWindowResize = this.onWindowResize.bind(this);
+    }
+
     render() {
         return (
             <div className="terminal" ref="terminal"></div>
@@ -11,21 +17,31 @@ export class WebTerm extends React.Component<any, any>{
     }
 
     componentDidMount(): void {
-        const cols = 500;
-        const rows = 10;
-
+        const div = this.refs["terminal"] as HTMLDivElement;
+        const cols = 80;
+        const rows = 20;
         const protocol = (location.protocol === 'https:') ? 'wss://' : 'ws://';
         let socketURL = protocol + location.hostname + ((location.port) ? (':' + location.port) : '') + '/terminals/';
-
         const self = this;
-
         fetch('/terminals?cols=' + cols + '&rows=' + rows, { method: 'POST' }).then(function (res) {
             res.text().then(function (pid) {
                 self.term = new ((window as any).Terminal)({
-                    cursorStyle: "underline",
-                    cursorBlink: true
+                    cursorBlink: true,
                 });
-                self.term.open(self.refs["terminal"] as HTMLDivElement);
+                self.term.open(div);
+                self.term.on('resize', function (size: any) {
+                    if (!pid) {
+                        return;
+                    }
+                    var cols = size.cols,
+                        rows = size.rows,
+                        url = '/terminals/' + pid + '/size?cols=' + cols + '&rows=' + rows;
+
+                    fetch(url, { method: 'POST' });
+                });
+
+                self.term.fit();
+                self.term.setOption("cursorStyle", "underline");
                 self.pid = pid;
                 socketURL += pid;
                 const socket = new WebSocket(socketURL);
@@ -35,12 +51,21 @@ export class WebTerm extends React.Component<any, any>{
                 }
             });
         });
+
+        window.addEventListener("resize", this.onWindowResize);
     }
 
     componentWillUnmount(): void {
         if (this.term) {
             this.term.destroy();
             this.term = null;
+        }
+        window.removeEventListener("resize", this.onWindowResize);
+    }
+
+    onWindowResize(): void {
+        if (this.term) {
+            this.term.fit();
         }
     }
 }
