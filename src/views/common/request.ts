233f1a34ffb483b40fc60d3c876420b25c8151b4
Tom Zhou <tomzhou@microsoft.com>
2017-11-14 14:32:34 -0800

    update script error

--- src/views/common/request.ts
+++ src/views/common/request.ts
@@ -1,10 +1,10 @@
 export function postJson(url: string, json: any) {
     return fetch(url, {
         method: "POST",
-        headers: {
-            'Accept': 'application/json, text/plain, */*',
-            'Content-Type': 'application/json'
-        },
+        headers: [
+            ['Accept', 'application/json, text/plain, */*'],
+            ['Content-Type','application/json'],
+        ],
         body: JSON.stringify(json)
     });
 };
